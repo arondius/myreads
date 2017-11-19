077d210c1e349ec7aded24979a30e02f0ac0c6a5
Arend Hosman <info@webb-online.nl>
2017-11-19 15:35:54 +0100

    feature: get bookList from API and pass to ListBooks component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import { Route } from 'react-router-dom'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {}
+  state = {
+    bookList: [],
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((bookList) => {
+      // Setting this.state.bookList to array with Books
+      this.setState({bookList});
+
+    })
+  }
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <ListBooks />
+          <ListBooks bookList={this.state.bookList}/>
         )}
         />
         <Route path="/search" render={() => (
