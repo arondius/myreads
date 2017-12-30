1dbbd278ff2dd6d186e3bf0e75706fa5291f6c6d
Arend Hosman <arend@winkwaves.com>
2017-12-30 14:39:38 +0100

    Refactor: Turn Book.js into functional stateless component

--- src/Book.js
+++ src/Book.js
@@ -1,48 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
 import BookShelfChanger from './BookShelfChanger';
 import PropTypes from 'prop-types';
 
-class Book extends Component {
-  renderAuthors() {
-    if(this.props.book.authors && this.props.book.authors.length > 0) {
-      const authors = this.props.book.authors.map(author => <li className="book-authors__author" key={author}>{author}</li>);
+const Book = (props) => {
+
+  const renderAuthors = () => {
+    if(props.book.authors && props.book.authors.length > 0) {
+      const authors = props.book.authors.map(author => <li className="book-authors__author" key={author}>{author}</li>);
       return (
         <ul className="book-authors">{authors}</ul>
       )
     }
   }
 
-  onBookShelfChanged(book, shelf) {
-    this.props.onChangeBookShelf(book, shelf);
+  const onBookShelfChanged = (book, shelf)  => {
+    props.onChangeBookShelf(book, shelf);
   }
 
-  render() {
-    const thumb = this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : 'https://books.google.nl/googlebooks/images/no_cover_thumb.gif';
-    return(
-      <li key={this.props.book.title}>
-        <div className="book">
-          <div className="book-top">
-            <div 
-              className="book-cover" 
-              style={{ 
-                width: 128, 
-                height: 193, 
-                backgroundImage: 'url(' + thumb + ')',
-                backgroundSize: 'cover'
-              }} 
-            />
-            <BookShelfChanger
-              onChangeBookShelf={this.onBookShelfChanged.bind(this)}
-              book={this.props.book}
-              currentShelf={this.props.currentShelf}
-            />
-          </div>
-          <div className="book-title">{this.props.book.title}</div>
-          {this.renderAuthors()}
+  const thumb = props.book.imageLinks ? props.book.imageLinks.thumbnail : 'https://books.google.nl/googlebooks/images/no_cover_thumb.gif';
+
+  return(
+    <li key={props.book.title}>
+      <div className="book">
+        <div className="book-top">
+          <div 
+            className="book-cover" 
+            style={{ 
+              width: 128, 
+              height: 193, 
+              backgroundImage: 'url(' + thumb + ')',
+              backgroundSize: 'cover'
+            }} 
+          />
+          <BookShelfChanger
+            onChangeBookShelf={onBookShelfChanged.bind(this)}
+            book={props.book}
+            currentShelf={props.currentShelf}
+          />
         </div>
-      </li>
-    )
-  }
+        <div className="book-title">{props.book.title}</div>
+        {renderAuthors()}
+      </div>
+    </li>
+  )
 }
 
 Book.propTypes = {
